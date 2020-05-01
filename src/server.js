1a525ebecb1aa2e6ef8d20c2874950d092639074
채원찬 <opace2341@kookmin.ac.kr>
2020-05-01 22:38:26 +0900

    경로별 router

--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-    console.log(1);
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
 });
 
-
-app.use(ctx => {
-    console.log(2);
+router.get('/about', (ctx, next) => {
+    ctx.body = '소개';
 });
 
-
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
+router.get('/about/:name', (ctx, next) => {
+    const { name } = ctx.params; 
+    ctx.body = name + '의 소개';
 });
 
-app.listen(4000, () => {
+router.get('/post', (ctx, next) => {
+    const { id } = ctx.request.query; 
+    if(id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4004, () => {
     console.log('heurm server is listening to port 4000');
 });
